304add4364b0816a4dac4745769751aa8adb26b8
EC2 Default User <ec2-user@ip-172-31-40-17.ap-northeast-1.compute.internal>
2019-12-15 15:48:38 +0000

    c11

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
-  
-  validates :name, presence: true, length: { maximum: 20 }
-  validates :description, length: { maximum: 300 }
+
+  validates :user_id, presence: true  
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { in: 5..300 }
 end
